2bf60c0b7aea239f65fb59c36f0e7f85036131b2
blernix <killian.lecrut@gmail.com>
2025-09-17 14:43:12 +0200

    modification balise google

--- src/app/layout.js
+++ src/app/layout.js
@@ -62,15 +62,19 @@ export default function RootLayout({ children }) {
       */}
       <head>
         {/* --- 2. AJOUTER LE SCRIPT GOOGLE TAG MANAGER --- */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-N62XPDLH');
-          `}
-        </Script>
+        <Script 
+    id="google-tag-manager" 
+    strategy="afterInteractive"
+    dangerouslySetInnerHTML={{
+      __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-N62XPDLH');
+      `,
+    }}
+  />
       </head>
 
       <body className={`${geistSans.variable} ${geistMono.variable} bg-background text-foreground`}>
